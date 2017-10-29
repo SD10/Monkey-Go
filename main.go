3737b1be6fc379369924d1ab8d84c7261fda4605
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-10-29 03:56:54 -0500

    Add basic REPL to print tokens

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
-)
+	"os"
+	"os/user"
+	"monkey/repl"
+)                                                                                                                             
 
 func main() {
-	fmt.Println("Monkey Programming Language")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Fell free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
