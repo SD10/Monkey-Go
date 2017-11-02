015a2218b20e95c16ba64a9883436208d9bcb678
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-11-02 00:59:23 -0500

    Add Node, Statement, Expression interfaces & Program root node

---
+++ ast/ast.go
@@ -0,0 +1,27 @@
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program will be the root node of every AST our parser produces
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
