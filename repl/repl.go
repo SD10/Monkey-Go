543746d6ce07b5945a52db81238e2d1bba8aa211
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-12-13 16:09:06 -0600

    Add parser to REPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"github.com/sd10/Monkey-Go/token"
 	"github.com/sd10/Monkey-Go/lexer"
+	"github.com/sd10/Monkey-Go/parser"
 )
 
 // PROMPT ...
@@ -24,9 +24,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
